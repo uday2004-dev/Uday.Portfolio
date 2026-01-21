71d38b2324a55ea1152f6335c7ccdef5fe78cb9b
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-01-21 18:16:06 +0530

    Bug fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
-import Skills from '../components/skills'
+// import Skills from '../components/skills'
+import Skills from '../components/Skills'
+
 import Projects from "../components/Projects"
 import Work from '../components/Work'
 import Contact from '../components/Contact'

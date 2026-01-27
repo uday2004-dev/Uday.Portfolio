a86ed6671b17246da0c726a6a865a700f85a174e
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-01-27 18:00:17 +0530

    correct few points in about us section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
             className='md:w-1/2'>
             <div className="rounded 2xl p-8">
               <h3 className='text-2xl font-semibold mb-6'>My Journey</h3>
-              <p className='text-gray-300 mb-6'>I'm a passionate full-stack developer with over 5 years of experience creating digital solutions for businesses around the world. My journey started with basic HTML/CSS websites and has evolved into building complex web applications with modern frameworks.</p>
+              <p className='text-gray-300 mb-6'>I'm a passionate full-stack developer with over 6 month of experience as an intern creating digital solutions for businesses around the world. My journey started with basic HTML/CSS websites and has evolved into building complex web applications with modern frameworks.</p>
               <p className='text-gray-300 mb-6' >When I'm not coding, you can find me exploring new technologies, contributing to open-source projects, or sharing my knowledge through tech blogs and tutorials. I believe in continuous learning and pushing the boundaries of what's possible on the web.</p>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
